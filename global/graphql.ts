9e28061744e005f944e1677ae92f46cb80f7e5f3
Ruben Vitt <r.vitt@fme.de>
2021-08-23 16:29:05 +0200

    ✨(general): some more implementations

--- global/graphql.ts
+++ global/graphql.ts
@@ -2,5 +2,9 @@ import { GraphQLClient } from 'graphql-request';
 
 export const apiGraphQLClient = new GraphQLClient(process.env.GRAPHCMS_URL, {
   headers:
-    process.env.NODE_ENV === 'development' ? { 'gcms-stage': 'DRAFT' } : {},
+    process.env.NODE_ENV === 'development' ||
+    process.env.VERCEL_ENV === 'preview' ||
+    process.env.VERCEL_ENV === 'development'
+      ? { 'gcms-stage': 'DRAFT' }
+      : {},
 });
