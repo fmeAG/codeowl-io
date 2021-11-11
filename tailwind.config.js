3edcc4d46d508d1b154925f883dd146c22070269
Jonas Kleiner <j.kleiner@fme.de>
2021-11-11 14:08:17 +0100

    added new customizable color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#e60000', //fme red
+        'primary-dark': '#b80000',
+        'secondary': '#DE004B', // fme pink
+        'dark-background': '#22232F' 
+      },
+    },
   },
   variants: {
     extend: {
